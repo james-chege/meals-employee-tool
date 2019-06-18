2fba3137c3563e0e23ff31f1bb131e00752b976e
dmaish <mainadaniel81@gmail.com>
2019-06-18 10:42:43 +0300

    choice-box built

--- src/components/querySection/index.jsx
+++ src/components/querySection/index.jsx
@@ -3,9 +3,47 @@ import './querySection.scss';
 
 export class QuerySection extends Component {
 
+    emails = ['jonathan.doeson@andela.com', 'jonathan.doeson@andela.com', 'jonathan.doeson@andela.com', 'jonathan.doeson@andela.com', 'jonathan.doeson@andela.com'];
+
+    renderChoiceBox (){
+        let visibility;
+
+        if (this.emails.length === 0) {
+            visibility = 'hidden';
+        } 
+        else {
+            visibility = 'display';
+        }
+
+        return(
+        <div className={`choice-box-${visibility}`}>
+            {this.emails.map( (emailItem) => (
+                <div className='email-item'> {emailItem} </div>
+            ) )}
+        </div>
+        )
+    }
+
+    renderEmailField (){
+        return(
+            <>
+                <div className='input-label'>
+                    Email
+                </div>
+                <div className='input-field' contenteditable='true'>
+                    
+                </div>
+                {this.renderChoiceBox()}
+            </>
+        )
+    }
+
     render() {
         return (
         <div className='query-section'>
+            <div className='email-input-container'>
+                {this.renderEmailField()}
+            </div>
         </div>
         );
     }
