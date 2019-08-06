5013548420e6ca2864eec4244cbae987bc25041d
chariss <charisschomba@gmail.com>
2019-08-06 10:51:43 +0300

    increase number of users per page

--- src/services/homePageService.js
+++ src/services/homePageService.js
@@ -4,7 +4,7 @@ import store from '../redux/store/store';
 
 export default class homePageService {
   static async getAllEmployees() {
-    const url = `${getBaseUrl()}/employees`;
+    const url = `${getBaseUrl()}/employees?perPage=${5000}`;
     return axios(
       {
         method: 'get',
